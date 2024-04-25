187aae3bcba7625a98dfe50bbb9bfb686b3f4f86
Error-404 <119523240+Abuzar-061@users.noreply.github.com>
2024-04-25 17:07:22 +0500

    Update ProdiaImage.py

--- ProdiaImage.py
+++ ProdiaImage.py
@@ -3,19 +3,25 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 prodia = Prodia(
-    api_key= "YOUR_API_KEY"
+    api_key= "YOUR_API_KEY" # Read Readme.md to get your own API KEY 
 )
 
-@app.route("/image-generation", methods=["POST"])
+@app.route("/image-generation", methods=["POST"])  # Define a route for image generation endpoint
 def image():
     try:
-        command = request.form.get('prompt')
-        job = prodia.sd.generate(prompt=command)
+        command = request.form.get('prompt')  # Get prompt from form data
+        
+        # Generate image using ProDia API with specified model
+        job = prodia.sd.generate(prompt=command, model="anything-v4.5-pruned.ckpt [65745d25]") # For Model Information Check the ModelListProdia.py File and get the model of your choice
+        
+        # Wait for the job to complete and get the result
         result = prodia.wait(job)
 
+        # Return the generated image URL in JSON response
         return jsonify({"Image_Url": result.image_url}), 200
     except Exception as e:
+        # Handle exceptions and return error message
         return jsonify({"Error": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)  # Run the Flask application
